de7a927f136f495de098b843ede3c5938cb6b6dc
Nazianah <nazianah.umme@gmail.com>
2024-07-02 11:30:36 +0400

    chore: Remove Firebase configuration file

---
+++ src/Firebase/Firebase.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTbnFT499onBueWNtkuy_SF2lOXZ1HTOI",
+  authDomain: "register-cd2cc.firebaseapp.com",
+  projectId: "register-cd2cc",
+  storageBucket: "register-cd2cc.appspot.com",
+  messagingSenderId: "430953567589",
+  appId: "1:430953567589:web:e39034cce3af92046afd8c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+export { app, auth };  
