3efef9028ec50ef8c2a251213b9bf665573d805e
Nazianah <nazianah.umme@gmail.com>
2024-07-02 13:51:29 +0400

    refactor: Remove Google sign-in option from login page

--- src/Components/auth/register/index.jsx
+++ src/Components/auth/register/index.jsx
@@ -1,105 +1,112 @@
-import React, { useState } from 'react'
-import { Navigate, Link, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../../contexts/authContext'
-import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth'
+import React, { useState } from 'react';
+import { Navigate, Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../contexts/authContext';
+import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth';
 
 const Register = () => {
+  const navigate = useNavigate();
+  const { userLoggedIn } = useAuth();
 
-    const navigate = useNavigate()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setconfirmPassword] = useState('')
-    const [isRegistering, setIsRegistering] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-    const { userLoggedIn } = useAuth()
-
-    const onSubmit = async (e) => {
-        e.preventDefault()
-        if(!isRegistering) {
-            setIsRegistering(true)
-            await doCreateUserWithEmailAndPassword(email, password)
-        }
+    if (password !== confirmPassword) {
+      setErrorMessage('Passwords do not match');
+      return;
     }
 
-    return (
-        <>
-            {userLoggedIn && (<Navigate to={'/home'} replace={true} />)}
+    setErrorMessage('');
+    setIsRegistering(true);
 
-            <main className="w-full h-screen flex self-center place-content-center place-items-center">
-                <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
-                    <div className="text-center mb-6">
-                        <div className="mt-2">
-                            <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">Create a New Account</h3>
-                        </div>
+    try {
+      await doCreateUserWithEmailAndPassword(email, password);
+      navigate('/home');
+    } catch (error) {
+      setErrorMessage(error.message);
+      setIsRegistering(false);
+    }
+  };
 
-                    </div>
-                    <form
-                        onSubmit={onSubmit}
-                        className="space-y-4"
-                    >
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                autoComplete='email'
-                                required
-                                value={email} onChange={(e) => { setEmail(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+  if (userLoggedIn) {
+    return <Navigate to="/home" replace />;
+  }
 
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Password
-                            </label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='new-password'
-                                required
-                                value={password} onChange={(e) => { setPassword(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
+        <div className="text-center mb-6">
+          <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">
+            Create a New Account
+          </h3>
+        </div>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <InputField
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <InputField
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            disabled={isRegistering}
+          />
+          <InputField
+            label="Confirm Password"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            disabled={isRegistering}
+          />
+          {errorMessage && (
+            <span className="text-red-600 font-bold">{errorMessage}</span>
+          )}
+          <button
+            type="submit"
+            disabled={isRegistering}
+            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${
+              isRegistering
+                ? 'bg-gray-300 cursor-not-allowed'
+                : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'
+            }`}
+          >
+            {isRegistering ? 'Signing Up...' : 'Sign Up'}
+          </button>
+          <div className="text-sm text-center">
+            Already have an account?{' '}
+            <Link to="/login" className="text-center text-sm hover:underline font-bold">
+              Continue
+            </Link>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
 
-                        <div>
-                            <label className="text-sm text-gray-600 font-bold">
-                                Confirm Password
-                            </label>
-                            <input
-                                disabled={isRegistering}
-                                type="password"
-                                autoComplete='off'
-                                required
-                                value={confirmPassword} onChange={(e) => { setconfirmPassword(e.target.value) }}
-                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
-                            />
-                        </div>
+const InputField = ({ label, type, value, onChange, required, disabled }) => (
+  <div>
+    <label className="text-sm text-gray-600 font-bold">{label}</label>
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      required={required}
+      disabled={disabled}
+      className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+    />
+  </div>
+);
 
-                        {errorMessage && (
-                            <span className='text-red-600 font-bold'>{errorMessage}</span>
-                        )}
-
-                        <button
-                            type="submit"
-                            disabled={isRegistering}
-                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
-                        >
-                            {isRegistering ? 'Signing Up...' : 'Sign Up'}
-                        </button>
-                        <div className="text-sm text-center">
-                            Already have an account? {'   '}
-                            <Link to={'/login'} className="text-center text-sm hover:underline font-bold">Continue</Link>
-                        </div>
-                    </form>
-                </div>
-            </main>
-        </>
-    )
-}
-
-export default Register
+export default Register;
